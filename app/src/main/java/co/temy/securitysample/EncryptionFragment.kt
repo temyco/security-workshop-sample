5ec8fb9a9348baff84a3ded2858a868439e20beb
Yakiv Mospan <yakiv.mospan@gmail.com>
2017-08-21 00:43:02 +0300

    Encryption Initialization
    1. Added Crypto class to work with encryption
    2. Added possibility to get Key Data info
    3. Added possibility to create simple synchronous key
    4. Added few test buttons to Encryption Tab

--- app/src/main/java/co/temy/securitysample/EncryptionFragment.kt
+++ app/src/main/java/co/temy/securitysample/EncryptionFragment.kt
@@ -2,12 +2,39 @@ package co.temy.securitysample;
 
 import android.os.Bundle
 import android.support.v4.app.Fragment
+import android.util.Log
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import co.temy.securitysample.encryption.Crypto
 
 class EncryptionFragment : Fragment() {
+
+    private var keyNumber = 0
+
     override fun onCreateView(inflater: LayoutInflater?, container: ViewGroup?, savedInstanceState: Bundle?): View? {
         return inflater?.inflate(R.layout.fragment_encryption, container, false)
+    }
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+    }
+
+    override fun onViewCreated(view: View?, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        view?.findViewById<View>(R.id.btnAliases)?.setOnClickListener { printAliases() }
+        view?.findViewById<View>(R.id.btnCreateSyncKey)?.setOnClickListener { createKey() }
+    }
+
+    private fun printAliases() {
+        val crypto = Crypto(activity.applicationContext)
+        crypto.getAllKeyAliases()
+                .sortedBy{it.creationDate}
+                .forEach { Log.i("EncryptionFragment", "${it} \n") }
+    }
+
+    private fun createKey() {
+        val crypto = Crypto(activity.applicationContext)
+        crypto.createSymmetricKey("Test-${++keyNumber}", false)
     }
 }
--- app/src/main/java/co/temy/securitysample/encryption/Crypto.kt
+++ app/src/main/java/co/temy/securitysample/encryption/Crypto.kt
@@ -1,23 +1,84 @@
 package co.temy.securitysample.encryption
 
+import android.annotation.TargetApi
 import android.content.Context
-import java.security.KeyStore
-import java.security.KeyStoreException
+import android.os.Build
+import android.security.keystore.KeyGenParameterSpec
+import android.security.keystore.KeyProperties
+import java.io.IOException
+import java.security.*
+import java.security.cert.CertificateException
+import java.util.*
+import javax.crypto.KeyGenerator
+import javax.crypto.SecretKey
 
 class Crypto(context: Context) {
 
     private val keyStore: KeyStore
+    private val keyGenerator: KeyGenerator?
 
     init {
         try {
             keyStore = KeyStore.getInstance("AndroidKeyStore")
+            keyStore.load(null)
         } catch (e: KeyStoreException) {
             throw RuntimeException("Failed to get an instance of KeyStore", e)
         }
+
+        try {
+            if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.M) {
+                keyGenerator = KeyGenerator.getInstance(KeyProperties.KEY_ALGORITHM_AES, "AndroidKeyStore")
+            } else {
+                keyGenerator = null
+            }
+        } catch (e: NoSuchAlgorithmException) {
+            throw RuntimeException("Failed to get an instance of KeyGenerator", e)
+        } catch (e: NoSuchProviderException) {
+            throw RuntimeException("Failed to get an instance of KeyGenerator", e)
+        }
     }
 
-    public fun getAllKeyAlieases(): List<String> {
-        return keyStore.aliases().toList()
+    fun getAllKeyAliases(): List<KeyData> {
+        return keyStore.aliases().toList().map { KeyData(it, keyStore.getCreationDate(it)) }
     }
 
+    @TargetApi(Build.VERSION_CODES.M)
+    fun createSymmetricKey(alias: String, invalidatedByBiometricEnrollment: Boolean): SecretKey? {
+        try {
+            // Set the alias of the entry in Android KeyStore where the key will appear
+            // and the constrains (purposes) in the constructor of the Builder
+
+            val builder = KeyGenParameterSpec.Builder(alias,
+                    KeyProperties.PURPOSE_ENCRYPT or KeyProperties.PURPOSE_DECRYPT)
+                    .setBlockModes(KeyProperties.BLOCK_MODE_CBC)
+//                    // Require the user to authenticate with a fingerprint to authorize every use
+//                    // of the key
+//                    .setUserAuthenticationRequired(true)
+                    .setEncryptionPaddings(KeyProperties.ENCRYPTION_PADDING_PKCS7)
+
+            // This is a workaround to avoid crashes on devices whose API level is < 24
+            // because KeyGenParameterSpec.Builder#setInvalidatedByBiometricEnrollment is only
+            // visible on API level +24.
+            // Ideally there should be a compat library for KeyGenParameterSpec.Builder but
+            // which isn't available yet.
+            if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.N) {
+                builder.setInvalidatedByBiometricEnrollment(invalidatedByBiometricEnrollment)
+            }
+            keyGenerator?.init(builder.build())
+            return keyGenerator?.generateKey()
+        } catch (e: NoSuchAlgorithmException) {
+            throw RuntimeException(e)
+        } catch (e: InvalidAlgorithmParameterException) {
+            throw RuntimeException(e)
+        } catch (e: CertificateException) {
+            throw RuntimeException(e)
+        } catch (e: IOException) {
+            throw RuntimeException(e)
+        }
+
+        return null
+    }
+
+    data class KeyData(val alias: String, val creationDate: Date)
 }
+

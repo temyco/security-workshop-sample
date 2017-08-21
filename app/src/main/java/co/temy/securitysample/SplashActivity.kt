7431708de89f2a01b9b6a08506f7517623321e77
Svyatoslav Hromyak <svyatoslav.hromyak@temy.co>
2017-08-21 17:40:42 +0300

    Implementation is ready for smoke test

--- app/src/main/java/co/temy/securitysample/SplashActivity.kt
+++ app/src/main/java/co/temy/securitysample/SplashActivity.kt
@@ -1,12 +1,28 @@
 package co.temy.securitysample
 
+import android.app.AlertDialog
 import android.content.Intent
 import android.os.Bundle
 import android.os.Handler
 import android.os.Looper
 import android.support.v7.app.AppCompatActivity
+import android.util.Log
+import android.view.View
+import android.widget.ProgressBar
+import android.widget.TextView
+import android.widget.Toast
+import com.google.android.gms.common.ConnectionResult
+import com.google.android.gms.common.GoogleApiAvailability
+import com.scottyab.safetynet.SafetyNetHelper
+import com.scottyab.safetynet.SafetyNetResponse
 
 class SplashActivity : AppCompatActivity() {
+
+    private val API_KEY = BuildConfig.GOOGLE_VERIFICATION_API_KEY
+    private val TAG:String = SplashActivity ::class.java.simpleName
+
+    private var progressBar:ProgressBar? = null
+    private var statusLabel:TextView? = null
 
     companion object {
         private val HOME_SCREEN_START_DELAY: Long = 1000
@@ -17,15 +33,95 @@ class SplashActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_splash)
-        checkSafeNet();
     }
 
     private fun checkSafeNet() {
+        progressBar = findViewById(R.id.progressBar)
+        statusLabel = findViewById(R.id.statusLabel)
+        if (GoogleApiAvailability.getInstance().isGooglePlayServicesAvailable(this) != ConnectionResult.SUCCESS) {
+            AlertDialog.Builder(this)
+                    .setTitle(R.string.warning_ttl)
+                    .setPositiveButton(R.string.exit_btn, { _, _ -> System.exit(0) })
+                    .setNegativeButton(R.string.proceed_btn, { _, _ -> goCheckWithLibrary() })
+                    .setMessage(R.string.gms_not_ready_msg)
+                    .create()
+                    .show()
 
+        } else {
+            goCheckWithLibrary ()
+        }
     }
 
     override fun onPostResume() {
         super.onPostResume()
+        checkSafeNet()
+    }
+
+    private fun goCheckWithLibrary () {
+        val safetyNetHelper = SafetyNetHelper(API_KEY)
+        safetyNetHelper.requestTest(this, object : SafetyNetHelper.SafetyNetWrapperCallback {
+            override fun error(errorCode: Int, errorMessage: String) {
+                handleError(errorCode, errorMessage)
+            }
+
+            override fun success(ctsProfileMatch: Boolean, basicIntegrity: Boolean) {
+                Log.d(TAG, "SafetyNet req success: ctsProfileMatch:$ctsProfileMatch and basicIntegrity, $basicIntegrity")
+                proceedWithSuccessfulResult(safetyNetHelper.lastResponse)
+            }
+        })
+    }
+
+    private fun proceedWithSuccessfulResult(safetyNetResponse: SafetyNetResponse) {
+        if (safetyNetResponse.isBasicIntegrity && safetyNetResponse.isCtsProfileMatch) {
+            Toast.makeText(this, R.string.secure_success_msg, Toast.LENGTH_LONG).show()
+            showLoading(false, getString(R.string.secure_success_msg))
+            goNextWithDelay ()
+        } else if (safetyNetResponse.isBasicIntegrity) {
+            AlertDialog.Builder(this)
+                    .setTitle(R.string.warning_ttl)
+                    .setPositiveButton(R.string.exit_btn, { _, _ -> System.exit(0) })
+                    .setNegativeButton(R.string.proceed_btn, { _, _ -> goNextWithDelay() })
+                    .setMessage(R.string.insecure_msg)
+                    .create()
+                    .show()
+            showLoading(false, getString(R.string.insecure_msg))
+
+        } else {
+            val builder = AlertDialog.Builder(this)
+                    .setTitle(R.string.warning_ttl)
+                    .setPositiveButton(R.string.exit_btn, { _, _ -> System.exit(0) })
+                    .setMessage(R.string.completely_insecure_msg)
+            showLoading(false, getString(R.string.completely_insecure_msg))
+            if (BuildConfig.DEBUG) {
+                builder.setNegativeButton(R.string.proceed_btn, { _, _ -> goNextWithDelay() })
+            }
+            builder.create().show()
+        }
+    }
+
+    private fun handleError(errorCode: Int, errorMessage: String) {
+        Log.d(TAG, "Something went wrong: " + errorCode)
+        showLoading(false, errorMessage)
+        AlertDialog.Builder(this)
+                .setTitle(R.string.warning_ttl)
+                .setPositiveButton(R.string.exit_btn, { _, _ -> System.exit(0) })
+                .setNegativeButton(R.string.proceed_btn, { _, _ -> goNextWithDelay() })
+                .setMessage(errorMessage)
+                .create()
+                .show()
+    }
+
+    private fun showLoading(show: Boolean, message: String) {
+        if (show) {
+            progressBar?.visibility = View.VISIBLE
+            statusLabel?.text = getString(R.string.check_environment_lab)
+        } else {
+            progressBar?.visibility = View.INVISIBLE
+            statusLabel?.text = message
+        }
+    }
+
+    private fun goNextWithDelay () {
         handler.postDelayed({ startHomeActivity() }, HOME_SCREEN_START_DELAY)
     }
 
@@ -34,7 +130,7 @@ class SplashActivity : AppCompatActivity() {
         handler.removeCallbacksAndMessages(null)
     }
 
-    fun startHomeActivity() {
+    private fun startHomeActivity() {
         val intent = Intent(this, HomeActivity::class.java)
         startActivity(intent)
         finish()
